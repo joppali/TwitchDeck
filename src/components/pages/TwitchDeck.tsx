f2d14fcdaab7ae9c0ce1656fbb13affcf016ea09
joppali <joppali.7878@gmail.com>
2019-05-26 16:01:38 +0900

    add emotion and mod header color

--- src/components/pages/TwitchDeck.tsx
+++ src/components/pages/TwitchDeck.tsx
@@ -1,14 +1,23 @@
-import React, { FC } from 'react';
+/** @jsx jsx */
+import React, {FC} from 'react';
 import ClipStream from '../organisms/ClipStream';
-import { Header } from 'semantic-ui-react';
+import {Header} from 'semantic-ui-react';
+import {css, jsx} from '@emotion/core';
+
+const headerCss = css`
+  background-color: #6441a4 !important;
+  border: 0px !important;
+  border-radius: 0px !important;
+  color: white !important;
+`;
 
 const TwitchDeck: FC = () => (
-  <>
-    <Header as="h3" block>
+  <div>
+    <Header as="h3" block css={headerCss}>
       TwitchDeck
     </Header>
     <ClipStream />
-  </>
+  </div>
 );
 
 export default TwitchDeck;
